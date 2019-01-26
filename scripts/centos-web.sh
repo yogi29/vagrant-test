c77ea0f3a07c8ea30e4daf1a09703c3d8f0adf7b
Yogi Sharma <yogi29aus@gmail.com>
2019-01-26 22:07:26 +1100

    updating web script

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -14,5 +14,10 @@ sudo systemctl start httpd
 #PHP
 yum install -y php php-cli php-common php-mysql
 
+#Download starter content from github
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/yogi29/vagrant-test/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/yogi29/vagrant-test/master/files/info.php
+
 #Restart Apache
 sudo systemctl restart httpd
