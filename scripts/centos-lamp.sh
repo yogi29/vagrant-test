5387471a73e0460df315a9fc0bf3d21e930aff8b
Yogi Sharma <yogi29aus@gmail.com>
2019-01-26 21:17:58 +1100

    updating mysql section in script

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 yum update -y --exclude=kernel
-yum install -y vim wget git unzip screen
+yum install -y vim wget git unzip net-tools screen
 
 # Install Apache
 yum install -y httpd httpd-devel httpd-tools
-systemctl enable httpd
-systemctl stop httpd
+sudo systemctl enable httpd
+sudo systemctl stop httpd
 
 rm -rf /var/www/html
 ln -s /vagrant /var/www/html
 
 chcon -R -t httpd_sys_content_t /var/www/html
-systemctl start httpd
+sudo systemctl start httpd
 
 #PHP
 yum install -y php php-cli php-common php-mysql
 
 #Mysql
 yum install -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-yum install mysql-community-server
-systemctl enable mysqld
-systemctl start mysqld
+sudo yum install -y mysql-community-server mysql-community-client MySQL-python
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
 
 #Download starter content from github
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/yogi29/vagrant-test/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/yogi29/vagrant-test/master/files/info.php
 
-
-
 #Restart Apache
-systemctl restart httpd
+sudo systemctl restart httpd
